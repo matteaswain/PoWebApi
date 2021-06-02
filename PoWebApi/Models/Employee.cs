167f9b11f35214e4a86e5b28cbea33ee8e6aef7c
MatteaAnn Swain <MMSwain@tql.com>
2021-06-02 12:36:54 -0400

    Add project files.

---
+++ PoWebApi/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoWebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public bool IsManager { get; set; } // will default to false so can leave it 
+
+
+
+        public Employee()
+        {
+            // default constructor
+        }
+    }
+}
